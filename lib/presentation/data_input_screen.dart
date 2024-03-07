345b39544f1f9909024e3738321c6584705e1ac4
Nino Handler <ninohandler@gmail.com>
2024-03-07 20:14:54 +0100

    Add placeholder input screen

---
+++ lib/presentation/data_input_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DataInputScreen extends StatelessWidget {
+  const DataInputScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
